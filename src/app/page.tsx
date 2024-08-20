902c7d7afd8702a295f692f535105b98b2f25a94
Leon <leonvoytkevichus@gmail.com>
2024-08-20 16:54:58 +0300

    Add authoComplete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState, useEffect, useMemo, ChangeEvent } from "react";
+import { useState, useEffect, useMemo, ChangeEvent, useRef } from "react";
 import { PetCard } from "./components/PetsCard";
 import axios from "axios";
 import { DetailedDog } from "./types/dog/type";
@@ -19,11 +19,28 @@ interface Cat extends Pet, DetailedCat {}
 
 export default function Home() {
   const [pets, setPets] = useState<Dog[]>([]);
-  const [filteredPets, setFilteredPets] = useState<Dog[]>([]);
+  const [selectedBreed, setSelectedBreed] = useState<string>("");
   const [searchTerm, setSearchTerm] = useState<string>("");
   const [isLoad, setIsLoad] = useState<boolean>(true);
   const [cats, setCats] = useState<Cat[]>([]);
+  const [showSuggestions, setShowSuggestions] = useState<boolean>(false);
+  const suggestionsRef = useRef<HTMLUListElement | null>(null);
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        suggestionsRef.current &&
+        !suggestionsRef.current.contains(event.target as Node)
+      ) {
+        setShowSuggestions(false);
+      }
+    };
 
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
   const fetchDogs = async (): Promise<void> => {
     try {
       const { data } = await axios.get<{ result: Dog[] }>("/api/dog");
@@ -59,21 +76,89 @@ export default function Home() {
 
   const handleSearchChange = (event: ChangeEvent<HTMLInputElement>): void => {
     const term = event.target.value;
+    if (!event.target.value) {
+      setSelectedBreed("");
+    }
     setSearchTerm(term);
+    setShowSuggestions(true);
+  };
+
+  const handleSuggestionClick = (name: string) => {
+    setSearchTerm(name);
+    setShowSuggestions(false);
+    setSelectedBreed(name);
   };
 
   const dogsArr = useMemo(() => pets, [pets.length]);
   const catsArr = useMemo(() => cats, [cats.length]);
+  const dogAuthoComplete = useMemo(
+    () => [...new Set([...pets.map((pet) => pet.name)])],
+    [pets.length]
+  );
+  const catAuthoComplete = useMemo(
+    () => [...new Set([...cats.map((pet) => pet.name)])],
+    [cats.length]
+  );
 
   return (
     <div className="pets-container p-8">
-      <input
-        type="text"
-        placeholder="Search breeds..."
-        value={searchTerm}
-        onChange={handleSearchChange}
-        className="mb-8 p-2 border border-gray-300 rounded w-full"
-      />
+      <div className="relative flex w-full items-center justify-center">
+        <input
+          type="search"
+          placeholder="Search breeds..."
+          value={searchTerm}
+          onChange={handleSearchChange}
+          onClick={() => setShowSuggestions(true)}
+          className="mb-8 p-2 border border-gray-300 rounded w-full"
+        />
+        {showSuggestions && (
+          <ul
+            className="absolute top-full left-0 w-full h-60 overflow-auto bg-white border border-gray-300 rounded shadow-lg z-10"
+            ref={suggestionsRef}
+          >
+            {dogAuthoComplete.length > 0 && (
+              <>
+                <h3 className="px-2 py-1 bg-gray-100 text-gray-900 text-xl">
+                  Dogs
+                </h3>
+                {dogAuthoComplete
+                  .filter((name) =>
+                    name.toLowerCase().includes(searchTerm.toLowerCase())
+                  )
+                  .map((name, index) => (
+                    <li
+                      key={index}
+                      className="p-2 cursor-pointer hover:bg-gray-100"
+                      onClick={() => handleSuggestionClick(name)}
+                    >
+                      {name}
+                    </li>
+                  ))}
+              </>
+            )}
+            {catAuthoComplete.length > 0 && (
+              <>
+                <h3 className="px-2 py-1 bg-gray-100 text-gray-900 text-xl">
+                  Cats
+                </h3>
+                {catAuthoComplete
+                  .filter((name) =>
+                    name.toLowerCase().includes(searchTerm.toLowerCase())
+                  )
+                  .map((name, index) => (
+                    <li
+                      key={index}
+                      className="p-2 cursor-pointer hover:bg-gray-100"
+                      onClick={() => handleSuggestionClick(name)}
+                    >
+                      {name}
+                    </li>
+                  ))}
+              </>
+            )}
+          </ul>
+        )}
+      </div>
       {isLoad ? (
         <Spinner />
       ) : (
@@ -83,11 +168,9 @@ export default function Home() {
             <div className="flex-1 flex flex-wrap gap-4 justify-center">
               {dogsArr.length > 0 ? (
                 dogsArr
-                  .filter((pet) => {
-                    return pet.name
-                      .toLowerCase()
-                      .includes(searchTerm.toLowerCase());
-                  })
+                  .filter((pet) =>
+                    pet.name.toLowerCase().includes(selectedBreed.toLowerCase())
+                  )
                   .map((pet, index) => (
                     <PetCard
                       key={index}
@@ -107,11 +190,9 @@ export default function Home() {
             <div className="flex-1 flex flex-wrap gap-4 justify-center">
               {catsArr.length > 0 ? (
                 catsArr
-                  .filter((pet) => {
-                    return pet.name
-                      .toLowerCase()
-                      .includes(searchTerm.toLowerCase());
-                  })
+                  .filter((pet) =>
+                    pet.name.toLowerCase().includes(selectedBreed.toLowerCase())
+                  )
                   .map((pet, index) => (
                     <PetCard
                       key={index}
